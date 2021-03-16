77e39009c3ed2b23b8b437eb809e75f4c616f053
Leonel Velez <leonel-14-velez@hotmail.com>
2021-03-16 02:31:45 -0500

    validando button bloqueado al agregar el carrito

--- src/app/home/detalle/detalle.component.ts
+++ src/app/home/detalle/detalle.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Producto, ProductoResponse } from 'src/app/model/producto';
 import { ProductosService } from 'src/app/services/productos.service';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-detalle',
@@ -15,15 +16,17 @@ export class DetalleComponent implements OnInit {
   public precio:string = "";
   public descripcion:string = "";
   public imagen = "";
+  public permiteAgg = true;
 
   constructor(
     private rout:Router,
     private productSer: ProductosService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private storage: Storage
   ) { 
     this.data = new Array<Producto>();
     let id :number =  Number(this.activatedRoute.snapshot.paramMap.get('id'))
-    this.productSer.productos().subscribe((resProdu:ProductoResponse) =>{     
+    this.productSer.productos().subscribe(async (resProdu:ProductoResponse) =>{     
            
       this.data = resProdu.data;
       
@@ -35,6 +38,14 @@ export class DetalleComponent implements OnInit {
       this.descripcion = this.dataObjet.detalle;
       
       this.imagen = "assets/"+this.dataObjet.imagen;
+      this.isAdd().then((x:boolean)=>{
+        this.permiteAgg = !x;
+        
+        
+      });
+      
+      
+      
       
     })
 
@@ -47,6 +58,49 @@ export class DetalleComponent implements OnInit {
   )
   {
     this.rout.navigateByUrl("home")
+  }
+
+  
+  isAdd()
+  {
+    return new Promise((resolve, reject)=>{
+      this.storage.get('prodSeleccionados').then((listaProdu:Producto[]) =>{
+        let listaProduAux = new Array<Producto>();
+        listaProduAux = listaProdu;
+        if(listaProduAux !== null)
+        {
+          if(listaProduAux.filter(x=>x.id === this.dataObjet.id).length === 0)
+          {
+            resolve(false) ;
+          }else{
+            resolve(true);
+          }
+        }else{
+          resolve(false);
+        }
+       
+      })  
+    })
+    
+  }
+
+  addToCar()
+  {
+    this.storage.get('prodSeleccionados').then((listaProdu:Producto[]) =>{
+        let listaProduAux = new Array<Producto>();
+        listaProduAux = listaProdu;
+        if(listaProduAux !== null)
+        {
+          if(listaProduAux.filter(x=>x.id === this.dataObjet.id).length === 0)
+          {
+            listaProduAux.push(this.dataObjet);
+            this.storage.set('prodSeleccionados', listaProduAux)
+          }  
+        }
+        
+        
+    })
+    
   }
 
 }
