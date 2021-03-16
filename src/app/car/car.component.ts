3a6541eb82d48acace64599e05311e15fb89aedf
Leonel Velez <leonel-14-velez@hotmail.com>
2021-03-16 04:23:34 -0500

    agregando logica de los checkbox

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -10,6 +10,7 @@ import { Producto } from '../model/producto';
 export class CarComponent implements OnInit {
 
   public data = new Array<Producto>();
+  public total : number = 0;
   constructor(
     private stora: Storage
   ) { 
@@ -17,6 +18,9 @@ export class CarComponent implements OnInit {
 
     this.stora.get('prodSeleccionados').then(respuesa =>{
       this.data = respuesa;
+      this.data.forEach((re:Producto)=>{
+        this.total = this.total  + Number(re.precio) * re.cantidad;
+      })
     })
   }
 
@@ -28,6 +32,66 @@ export class CarComponent implements OnInit {
     
     return 'assets/'+url;
   }
+
+  changeProduct(event, valor, cantidad)
+  {
+    
+    if(event.target.checked)
+    {
+      this.sumarTotal(valor*cantidad);
+    }else{
+      this.restarTotal(valor*cantidad);
+    }
+    
+  }
+
+  sumarCantidad($event, id)
+  {
+    this.data.forEach(val =>{
+          if(val.id === id)
+          {
+            if(val.check)
+            {
+              val.cantidad = val.cantidad + 1;
+              this.sumarTotal(Number(val.precio))
+            }
+            
+
+          }
+          
+    })
+  }
+
+  restarCantidad($event, id)
+  {
+    this.data.forEach(val =>{
+      if(val.id === id)
+      {
+        if(val.cantidad > 1)
+        {
+          if(val.check)
+          {
+            val.cantidad = val.cantidad - 1;
+            this.restarTotal(Number(val.precio))
+          }
+          
+        }
+        
+      }      
+  })
+  }
+  sumarTotal(val:number)
+  {
+    this.total = this.total + Number(val);
+
+  }
+
+  restarTotal(val:number)
+  {
+    this.total = this.total - Number(val);
+
+  }
+  
 
 
 }
